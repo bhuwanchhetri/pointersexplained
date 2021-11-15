aa3f510de2f78347858b35a52a250003926b2a8b
Bhuwan Chhetri <bhuwan.chhetri1@gmail.com>
2021-11-15 09:19:56 +0200

    Add project files.

---
+++ TEST3/TEST3.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+
+    //pointer explained
+
+    string name = "MY Learning";
+    string* ptr = &name;
+    cout << "value in variable:" << name; 
+    cout << "\n";
+    cout << "Address of variable:" << ptr; //calls address
+    cout << "\n";
+    cout << "Address of variable:" << &name; //call address
+
+
+
+
+
+        return 0;
+}
+
